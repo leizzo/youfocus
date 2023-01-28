fa34ee643e3e0fc37976a09b05c08dde9efde585
leizzo <ismail.bahtiyar@gmail.com>
2023-01-28 18:02:12 +0300

    gitflow-feature-stash: typings

--- packages/storage/src/settings.storage.jsx
+++ packages/storage/src/settings.storage.jsx
@@ -1,10 +1,19 @@
 import { createChromeStorageStateHookLocal } from 'use-chrome-storage';
-
+/**
+ * Chrome Storage Key
+ * @typedef {import('@yt-focus/types/src/storage.type').SETTINGS_KEY}
+ */
 const SETTINGS_KEY = 'settings';
+/**
+ * Chrome Storage Initial Value
+ * @typedef {import('@yt-focus/types/src/storage.type').SETTINGS_INITIAL_VALUE}
+ */
 const INITIAL_VALUE = {
   isSidebarDisabled: false,
   isShortsDisabled: false,
   isCommentDisabled: false,
 };
-
+/**
+ * Settings Store Hook
+ */
 export const useSettingsStore = createChromeStorageStateHookLocal(SETTINGS_KEY, INITIAL_VALUE);
--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,11 +1,10 @@
 export { Button } from './components/Button';
 export { SwitchBase } from './components/Switch';
-
+export { classNames } from './utils/classNames';
+export {
+  ELEMENT_CLASSNAMES, ELEMENT_SELECTOR, observeElementInTheDOM, observeUrlChange,
+} from './utils/elements';
 /**
  * Utils
  */
 export { getMessage } from './utils/i18n';
-export { classNames } from './utils/classNames';
-export {
-  ELEMENT_CLASSNAMES, ELEMENT_SELECTOR, observeUrlChange, observeElementInTheDOM,
-} from './utils/elements';
