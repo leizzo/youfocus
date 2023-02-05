0ae8f2cc525baa1326465fb047dd545824ae49b5
leizzo <ismail.bahtiyar@gmail.com>
2023-02-06 00:53:06 +0300

    feat: added youfocus brand name

--- packages/content/src/components/footer.component.jsx
+++ packages/content/src/components/footer.component.jsx
@@ -2,7 +2,11 @@ function Footer() {
   return (
     <div className="w-full absolute bottom-4 left-0 justify-center flex">
       <span className="text-xs">
-        { __APP_VERSION__ }
+        <a href="https://youfocus.vercel.app" target="_blank" rel="noreferrer">@youfocus</a>
+        <br />
+        <span className="opacity-50">
+          { __APP_VERSION__ }
+        </span>
       </span>
     </div>
   );
